0df9b3b42e579ae74e2b70c3b78b1d2ca52ecd8f
chenway <1099481194@qq.com>
2025-09-07 15:22:09 +0800

    优化

--- main.py
+++ main.py
@@ -73,6 +73,6 @@ if __name__ == '__main__':
     app = create_app()
     app.run(
         host='0.0.0.0',
-        port=5001,  # 使用不同端口避免冲突
+        port=8000,  # 使用不同端口避免冲突
         debug=True
     )
